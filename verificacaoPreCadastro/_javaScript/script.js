5f65a0284490bf4ba3940b932126208faefe12f7
gpaferrari <79278499+gpaferrari@users.noreply.github.com>
2021-12-20 09:43:57 -0300

    Add files via upload

---
+++ verificacaoPreCadastro/_javaScript/script.js
@@ -0,0 +1,9 @@
+// Manipular o botão 
+
+function valido(){
+    alert("Obrigado por nós escolher, agora é só preencher o formulario!");
+}
+
+function invalido(){
+    alert("Vocẽ não tem idade suficiente para ir para a proxima pagina.");
+}
